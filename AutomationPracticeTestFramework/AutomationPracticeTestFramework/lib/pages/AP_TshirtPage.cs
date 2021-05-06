ff79d6a6d3b79939d72a48295a156bc8859896b9
BarathNallusamy <nallu.baray@gmail.com>
2021-05-06 16:39:35 +0100

    Add product to shopping cart successfully

---
+++ AutomationPracticeTestFramework/AutomationPracticeTestFramework/lib/pages/AP_TshirtPage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+
+namespace AutomationPracticeTestFramework
+{
+    public class AP_TshirtPage
+    {
+        public IWebDriver SeleniumDriver { get; }
+        private string TshirtPageUrl = "http://automationpractice.com/index.php?id_category=5&controller=category";
+
+        private IWebElement _productView => SeleniumDriver.FindElement(By.LinkText("More"));
+        private IWebElement _productHover => SeleniumDriver.FindElement(By.CssSelector(".lnk_view > span"));
+        private IWebElement _addToCart => SeleniumDriver.FindElement(By.CssSelector(".exclusive > span"));
+        private IWebElement _successMessage => SeleniumDriver.FindElement(By.ClassName("layer_cart_product"));
+
+
+
+        public AP_TshirtPage(IWebDriver seleniumDriver)
+        {
+            SeleniumDriver = seleniumDriver;
+        }
+
+        public void VisitTshirtCatPage()
+        {
+            SeleniumDriver.Navigate().GoToUrl(TshirtPageUrl);
+        }
+
+        public void ClickProductViewBtn()
+        {
+            Actions action = new Actions(SeleniumDriver);
+            action.MoveToElement(_productHover).Perform();
+            Thread.Sleep(5000);
+            _productView.Click();
+        }
+
+        public void AddProductToCart()
+        {
+            _addToCart.Click();
+            Thread.Sleep(5000);
+        }
+
+        public string GetSuccessfullyAddedMessage()
+        {
+            return _successMessage.Text;
+        }
+    }
+}
