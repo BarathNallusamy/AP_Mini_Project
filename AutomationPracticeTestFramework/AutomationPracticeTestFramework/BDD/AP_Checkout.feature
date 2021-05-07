cdc1196a24b7650adc10976b1c3aa4c1fb18fd3a
Albert Gabrychowicz <albertg980@gmail.com>
2021-05-07 10:30:30 +0100

    Test for checking out

---
+++ AutomationPracticeTestFramework/AutomationPracticeTestFramework/BDD/AP_Checkout.feature
@@ -0,0 +1,19 @@
+﻿Feature: AP_Checkout
+	Simple calculator for adding two numbers
+
+@happy
+Scenario: Whole journey 
+Given I am on the homepage and I sign in and add t shirt to the cart
+And go to checkout page
+And click proceed to checkout (to step 2)
+
+
+
+
+Then I move to the Tshirt results page
+
+@happy
+Scenario: checkout go to step 2
+Given I am on the summary tab of the checkout
+When I click proceed to checkout 
+Then I proceed to the sign in tab
--- AutomationPracticeTestFramework/AutomationPracticeTestFramework/lib/pages/AP_HomePage.cs
+++ AutomationPracticeTestFramework/AutomationPracticeTestFramework/lib/pages/AP_HomePage.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace AutomationPracticeTestFramework
 {
@@ -7,6 +9,9 @@ namespace AutomationPracticeTestFramework
         public IWebDriver SeleniumDriver { get; }
         private string HomePageUrl = AppConfigReader.BaseUrl;
         private IWebElement _signinLink => SeleniumDriver.FindElement(By.LinkText("Sign in"));
+        private IWebElement _tshirtLink => SeleniumDriver.FindElement(By.LinkText("T-shirts"));
+        private IWebElement _cartLink => SeleniumDriver.FindElement(By.XPath("//header[@id='header']/div[3]/div/div/div[3]/div/a"));
+        private IWebElement _checkoutLink => SeleniumDriver.FindElement(By.Id("button_order_cart"));
         public AP_HomePage(IWebDriver seleniumDriver)
         {
             SeleniumDriver = seleniumDriver;
@@ -19,6 +24,19 @@ namespace AutomationPracticeTestFramework
         public void ClickSigninLink()
         {
             _signinLink.Click();
+        }
+
+        public void ClickTshirtLink()
+        {
+            _tshirtLink.Click();
+        }
+
+        public void ClickOnTheCart()
+        {
+            Actions action = new Actions(SeleniumDriver);
+            action.MoveToElement(_cartLink).Perform();
+            Thread.Sleep(5000);
+            _checkoutLink.Click();
         }
     }
 }
