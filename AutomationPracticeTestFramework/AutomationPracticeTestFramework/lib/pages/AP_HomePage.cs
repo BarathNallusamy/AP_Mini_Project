9610b5a01977031cbb0ef6dc4da24822a3eb1d79
Albert Gabrychowicz <albertg980@gmail.com>
2021-05-07 12:23:04 +0100

    Revert "Revert "Revert "Test for checking out"""
    This reverts commit 8b671838484617501bcb720a0c32ab5f3ad8d1bc.

--- AutomationPracticeTestFramework/AutomationPracticeTestFramework/lib/pages/AP_HomePage.cs
+++ AutomationPracticeTestFramework/AutomationPracticeTestFramework/lib/pages/AP_HomePage.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Interactions;
+﻿using OpenQA.Selenium;
 
 namespace AutomationPracticeTestFramework
 {
@@ -9,9 +7,6 @@ namespace AutomationPracticeTestFramework
         public IWebDriver SeleniumDriver { get; }
         private string HomePageUrl = AppConfigReader.BaseUrl;
         private IWebElement _signinLink => SeleniumDriver.FindElement(By.LinkText("Sign in"));
-        private IWebElement _tshirtLink => SeleniumDriver.FindElement(By.LinkText("T-shirts"));
-        private IWebElement _cartLink => SeleniumDriver.FindElement(By.XPath("//header[@id='header']/div[3]/div/div/div[3]/div/a"));
-        private IWebElement _checkoutLink => SeleniumDriver.FindElement(By.Id("button_order_cart"));
         public AP_HomePage(IWebDriver seleniumDriver)
         {
             SeleniumDriver = seleniumDriver;
@@ -24,19 +19,6 @@ namespace AutomationPracticeTestFramework
         public void ClickSigninLink()
         {
             _signinLink.Click();
-        }
-
-        public void ClickTshirtLink()
-        {
-            _tshirtLink.Click();
-        }
-
-        public void ClickOnTheCart()
-        {
-            Actions action = new Actions(SeleniumDriver);
-            action.MoveToElement(_cartLink).Perform();
-            Thread.Sleep(5000);
-            _checkoutLink.Click();
         }
     }
 }
